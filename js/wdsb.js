7efec99e0a80531f7ef4f0c4b667ad6b58653e68
victor <victor@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-03-11 06:27:53 +0000

    added js check for css Transform support. if supported, adds .transform to html elem
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/sharebox@8734 f53fe46e-8c02-459b-9fdd-93e75771b278

--- js/wdsb.js
+++ js/wdsb.js
@@ -162,4 +162,30 @@ function assignDimensions () {
 	}
 }
 
+/* http://net.tutsplus.com/tutorials/html-css-techniques/quick-tip-detect-css-support-in-browsers-with-javascript/ */
+var supports = (function() {  
+   var div = document.createElement('div'),  
+      vendors = 'Khtml Ms O Moz Webkit'.split(' '),  
+      len = vendors.length;  
+  
+   return function(prop) {  
+      if ( prop in div.style ) return true;  
+  
+      prop = prop.replace(/^[a-z]/, function(val) {  
+         return val.toUpperCase();  
+      });  
+  
+      while(len--) {  
+         if ( vendors[len] + prop in div.style ) {   
+            return true;  
+         }   
+      }  
+      return false;  
+   };  
+})();  
+  
+if ( supports('transform') ) { 
+   document.documentElement.className += ' transform';  
+}  
+
 })(jQuery);
